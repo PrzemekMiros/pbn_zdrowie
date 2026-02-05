31fbed5c4cfe06d8d7f33d584f3526bf37470134
PrzemekMiros <kontakt@futurewebstudio.pl>
2026-02-05 18:25:11 +0100

    Auto blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import netlify from '@astrojs/netlify';
 import tailwindcss from '@tailwindcss/vite';
 import robots from 'astro-robots-txt';
 import sitemap from 'astro-sitemap';
-import min from 'astro-min';
 import pagefind from 'astro-pagefind';
 
 export default defineConfig({
@@ -19,9 +18,6 @@ export default defineConfig({
         { userAgent: '*', allow: '/' }
       ],
       sitemap: 'https://zdrowiebezpaniki.pl/sitemap-index.xml'
-    }),
-    min({
-      minify_js: false
     }),
     pagefind()
   ],
